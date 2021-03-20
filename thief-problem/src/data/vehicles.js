5855a797f0a3875c3d2b4cd815dc59d3f63556a0
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-20 15:03:42 -0300

    Add vehicles data.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

---
+++ thief-problem/src/data/vehicles.js
@@ -0,0 +1,22 @@
+export const vehicles = [
+  {
+    id: 1,
+    name: '3/4',
+    space: 29,
+  },
+  {
+    id: 2,
+    name: 'Toco',
+    space: 44,
+  },
+  {
+    id: 3,
+    name: 'Truck',
+    space: 58,
+  },
+  {
+    id: 4,
+    name: 'Carreta',
+    space: 97,
+  },
+];
