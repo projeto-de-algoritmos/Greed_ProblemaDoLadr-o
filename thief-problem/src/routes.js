3145672ef316e4e288b548d5b49c37c4ac758a25
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-21 02:54:29 -0300

    Update items and remove Result page.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

--- thief-problem/src/routes.js
+++ thief-problem/src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sculptures from './pages/Sculptures';
-import Results from './pages/Results';
 
 const Routes = () => {
   return (
@@ -10,7 +9,6 @@ const Routes = () => {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/sculptures/:id" component={Sculptures} />
-        <Route path="/results" component={Results} />
       </Switch>
     </BrowserRouter>
   );
